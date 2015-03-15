69e30b07d04c52ce90b3bee5dc73b8d8ef0e709c
Dale Wijnand <dale.wijnand@gmail.com>
2015-03-15 12:06:05 +0000

    Tweak initialCommands for nicer examples.

--- src/main/resources/initialCommands.scala
+++ src/main/resources/initialCommands.scala
@@ -1,8 +1,24 @@
 import net.mox9.tabular._
 
-val m = Map("a" -> 1, "bb" -> 2, "ccc" -> 3)
+val m = scala.collection.immutable.ListMap(
+     "a" -> 1,
+    "bb" -> 2,
+   "ccc" -> 3,
+  "dddd" -> 4,
+    "ee" -> 5,
+     "f" -> 6
+)
 
-val mm = Map("a" -> Seq(1), "bb" -> Seq(2, 2), "ccc" -> Seq(3, 3, 3))
+val headers = scala.collection.immutable.ListMap(
+       "Cache-Control" -> Seq("no-cache", "no-store"),
+        "Content-Type" -> Seq("text/html; charset=UTF-8"),
+            "Location" -> Seq("http://www.google.co.uk/?gfe_rd=cr&ei=E5L3VJG_JoKr7AaKtoDICw"),
+      "Content-Length" -> Seq("261"),
+                "Date" -> Seq("Wed, 04 Mar 2015 23:15:31 GMT"),
+              "Server" -> Seq("GFE/2.0"),
+  "Alternate-Protocol" -> Seq("80:quic,p=0.08")
+)
+
 
 case class Ver(major: Int, minor: Int, patch: Int) {
   override def toString = s"$major.$minor.$patch"
@@ -12,6 +28,10 @@ case class Dep(g: String, a: String, v: Ver) {
 }
 
 val deps = Seq(
-  Dep("com.example.foo", "foo-server", Ver(1, 2, 3)),
+  Dep("com.example.foo", "foo-server", Ver(1, 2, 103)),
   Dep("com.acme.bar", "bar-scala-sdk", Ver(12, 0, 1))
 )
+
+implicit class AnyW[A](private val x: A) extends AnyVal {
+  def >> : Unit = println(x)
+}
