9394d262beaa29052b5b827d6736e510923b41c0
Dale Wijnand <dale.wijnand@gmail.com>
2015-12-28 17:09:29 +0000

    Change package to tabular

--- src/main/scala/tabular.scala
+++ src/main/scala/tabular.scala
@@ -1,4 +1,4 @@
-package net.mox9.tabular
+package tabular
 
 sealed trait TextAlign extends Any   { def alignBy(width: Int): String }
 case object LAlign extends TextAlign { def alignBy(width: Int) = width.lalign }
