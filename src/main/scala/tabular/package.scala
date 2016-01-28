0a397c6a78dd6e679b2d58eb6a861fdf4f9258d0
Dale Wijnand <dale.wijnand@gmail.com>
2016-01-28 21:44:26 +0000

    Move classes out of being directly defined in package object

--- src/main/scala/tabular/package.scala
+++ src/main/scala/tabular/package.scala
@@ -11,11 +11,11 @@ sealed trait StrWithAlignOps extends Any with StrWithAlign {
   def str: String
   def align: TextAlign
 }
-class LString(val str: String) extends AnyVal with StrWithAlignOps {
+final case class LString(val str: String) extends AnyVal with StrWithAlignOps {
   def align = LAlign
   def +(s: String) = new LString(str + s)
 }
-class RString(val str: String) extends AnyVal with StrWithAlignOps {
+final case class RString(val str: String) extends AnyVal with StrWithAlignOps {
   def align = RAlign
   def +(s: String) = new RString(str + s)
 }
@@ -24,11 +24,11 @@ object StrWithAlign {
   implicit def liftOps(x: StrWithAlign): StrWithAlignOps = x match { case y: StrWithAlignOps => y }
 }
 
-final class TravOnceWithMaxOpt[A](private val xs: TraversableOnce[A]) extends AnyVal {
+final case class TravOnceWithMaxOpt[A](private val xs: TraversableOnce[A]) extends AnyVal {
   def maxOpt[B >: A](implicit cmp: Ordering[B]): Option[B] = if (xs.isEmpty) None else Some(xs max cmp)
 }
 
-final class TravKVWithTabular[K, V](private val xs: Traversable[(K, V)]) extends AnyVal {
+final case class TravKVWithTabular[K, V](private val xs: Traversable[(K, V)]) extends AnyVal {
   def maxKeyLen = xs.toIterator.map(_._1.toString.length).maxOpt
   def tabularkv = {
     xs.maxKeyLen.fold(Nil: Traversable[String]) { len =>
@@ -39,7 +39,7 @@ final class TravKVWithTabular[K, V](private val xs: Traversable[(K, V)]) extends
   def showkv() = tabularkv foreach println
 }
 
-final class TravKVsWithTabular[K, V](private val xs: Traversable[(K, Traversable[V])]) extends AnyVal {
+final case class TravKVsWithTabular[K, V](private val xs: Traversable[(K, Traversable[V])]) extends AnyVal {
   def tabularkvs = {
     xs.maxKeyLen.fold(Nil: Traversable[String]) { len =>
       val fmt = s"%${len}s %s"
@@ -56,50 +56,63 @@ trait Tabular {
   implicit def travKVsWithTabular[K, V](xs: Traversable[(K, Traversable[V])]) : TravKVsWithTabular[K, V] = new TravKVsWithTabular[K, V](xs)
 }
 
-object `package` extends Tabular {
-  implicit class IntWithAlign(private val x: Int) extends AnyVal {
-    def lalign: String = if (x == 0) "%s" else s"%-${x}s"
-    def ralign: String = if (x == 0) "%s" else s"%${x}s"
+final case class IntWithAlign(private val x: Int) extends AnyVal {
+  def lalign: String = if (x == 0) "%s" else s"%-${x}s"
+  def ralign: String = if (x == 0) "%s" else s"%${x}s"
+}
+
+final case class AnyWithTextAlign[A](private val x: A) extends AnyVal {
+  def lj = new LString(x.toString)
+  def rj = new RString(x.toString)
+}
+
+final case class TraversableW[A](private val xs: Traversable[A]) extends AnyVal {
+  def tabular(columns: (A => StrWithAlign)*): String = {
+    if (xs.isEmpty || columns.isEmpty) ""
+    else {
+      val values = xs.toVector
+      val functions = columns.toVector
+
+      def rows = values map (x => functions map (f => f(x).str))
+      def cols = functions map (f => values map (x => f(x).str))
+      def renderLines = {
+        def widths = cols map (_ map (_.length) max)
+        def aligns = functions map (_(values.head).align)
+        val rowFormat = (widths, aligns).zipped map ((width, align) => align alignBy width) mkString " "
+        rows map (row => rowFormat.format(row: _*))
+      }
+      renderLines mkString "\n"
+    }
   }
+}
+
+final case class TraversableKVW[K, V](private val xs: Traversable[(K, V)]) extends AnyVal {
+  def showkv(implicit vShow: V => String = _.toString): String =
+    xs tabular (_._1.rj + ":", kv => vShow(kv._2))
+}
+
+final case class TraversableKMVW[K, V](private val xs: Traversable[(K, Traversable[V])]) extends AnyVal {
+  def showkvs(implicit mvShow: Traversable[V] => String = _ mkString ", "): String =
+    xs showkv mvShow
+}
+
+final case class AnyWith_>>[A](private val x: A) extends AnyVal {
+  def >>() = println(x)
+}
+
+object `package` extends Tabular {
+  implicit def intWithAlign(x: Int): IntWithAlign = new IntWithAlign(x)
+
   def lalign(width: Int): String = width.lalign
   def ralign(width: Int): String = width.ralign
 
-  implicit class AnyWithTextAlign[A](private val x: A) extends AnyVal {
-    def lj = new LString(x.toString)
-    def rj = new RString(x.toString)
-  }
+  implicit def anyWithTextAlign[A](x: A): AnyWithTextAlign[A] = new AnyWithTextAlign[A](x)
 
-  implicit class TraversableW[A](private val xs: Traversable[A]) extends AnyVal {
-    def tabular(columns: (A => StrWithAlign)*): String = {
-      if (xs.isEmpty || columns.isEmpty) ""
-      else {
-        val values = xs.toVector
-        val functions = columns.toVector
+  implicit def traversableW[A](xs: Traversable[A]): TraversableW[A] = new TraversableW[A](xs)
 
-        def rows = values map (x => functions map (f => f(x).str))
-        def cols = functions map (f => values map (x => f(x).str))
-        def renderLines = {
-          def widths = cols map (_ map (_.length) max)
-          def aligns = functions map (_(values.head).align)
-          val rowFormat = (widths, aligns).zipped map ((width, align) => align alignBy width) mkString " "
-          rows map (row => rowFormat.format(row: _*))
-        }
-        renderLines mkString "\n"
-      }
-    }
-  }
+  implicit def traversableKVW[K, V](xs: Traversable[(K, V)]): TraversableKVW[K, V] = new TraversableKVW[K, V](xs)
 
-  implicit class TraversableKVW[K, V](private val xs: Traversable[(K, V)]) extends AnyVal {
-    def showkv(implicit vShow: V => String = _.toString): String =
-      xs tabular (_._1.rj + ":", kv => vShow(kv._2))
-  }
+  implicit def traversableKMVW[K, V](xs: Traversable[(K, Traversable[V])]): TraversableKMVW[K, V] = new TraversableKMVW[K, V](xs)
 
-  implicit class TraversableKMVW[K, V](private val xs: Traversable[(K, Traversable[V])]) {
-    def showkvs(implicit mvShow: Traversable[V] => String = _ mkString ", "): String =
-      xs showkv mvShow
-  }
-
-  implicit class AnyWith_>>[A](private val x: A) extends AnyVal {
-    def >>() = println(x)
-  }
+  implicit def anyWith_>>[A](x: A): AnyWith_>>[A] = new AnyWith_>>[A](x)
 }
