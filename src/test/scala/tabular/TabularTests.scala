810432bf7379c1e2633259312f267be34419ff7d
Dale Wijnand <dale.wijnand@gmail.com>
2016-02-12 02:30:55 +0000

    Add tests for showkvs, showPs & showM

--- src/test/scala/tabular/TabularTests.scala
+++ src/test/scala/tabular/TabularTests.scala
@@ -11,12 +11,32 @@ object TabularTestAux {
     "e" -> 5
   )
 
+  val headers = scala.collection.immutable.ListMap(
+         "Cache-Control" -> Seq("no-cache", "no-store"),
+          "Content-Type" -> Seq("text/html; charset=UTF-8"),
+              "Location" -> Seq("http://www.google.co.uk/?gfe_rd=cr&ei=E5L3VJG_JoKr7AaKtoDICw"),
+        "Content-Length" -> Seq("261"),
+                  "Date" -> Seq("Wed, 04 Mar 2015 23:15:31 GMT"),
+                "Server" -> Seq("GFE/2.0"),
+    "Alternate-Protocol" -> Seq("80:quic,p=0.08")
+  )
+
   final case class Ver(x: Int, y: Int, z: Int)       { override def toString = s"$x.$y.$z"     }
   final case class Dep(g: String, a: String, v: Ver) { override def toString = s"$g % $a % $v" }
 
   val deps = Seq(
     Dep("com.example.foo", "foo-server", Ver(1, 2, 103)),
     Dep("com.acme.bar", "bar-scala-sdk", Ver(12, 0, 1))
+  )
+
+  final case class Foo(i: Int, str: String, bool: Boolean)
+
+  val xs = Seq(Foo(23, "foo", true), Foo(0, "", false), Foo(999, "quiz", true))
+
+  val matrix = Seq(
+    Seq(1, 2, 3),
+    Seq(4, 5, 6),
+    Seq(7, 8, 9)
   )
 }
 
@@ -33,11 +53,40 @@ object TabularTests extends TestSuite {
       val expect = Seq(l1, l2, l3, l4, l5) mkString "\n"
       assert(actual == expect)
     }
+    'showkvs {
+      val actual = headers.showkvs mkString "\n"
+      val l1 = "     Cache-Control: no-cache, no-store                                          "
+      val l2 = "      Content-Type: text/html; charset=UTF-8                                    "
+      val l3 = "          Location: http://www.google.co.uk/?gfe_rd=cr&ei=E5L3VJG_JoKr7AaKtoDICw"
+      val l4 = "    Content-Length: 261                                                         "
+      val l5 = "              Date: Wed, 04 Mar 2015 23:15:31 GMT                               "
+      val l6 = "            Server: GFE/2.0                                                     "
+      val l7 = "Alternate-Protocol: 80:quic,p=0.08                                              "
+      val expect = Seq(l1, l2, l3, l4, l5, l6, l7) mkString "\n"
+      assert(actual == expect)
+    }
     'tabular {
       val actual = deps tabular (_.g.rj, "%%", _.a.cj, '%', _.v) mkString "\n"
       val l1 = "com.example.foo %%  foo-server   % 1.2.103"
       val l2 = "   com.acme.bar %% bar-scala-sdk % 12.0.1 "
       val expect = Seq(l1, l2) mkString "\n"
+      assert(actual == expect)
+    }
+    'showPs {
+      val actual = xs.showPs mkString "\n"
+      val l0 = "  i  str  bool"
+      val l1 = " 23  foo  true"
+      val l2 = "  0      false"
+      val l3 = "999 quiz  true"
+      val expect = Seq(l0, l1, l2, l3) mkString "\n"
+      assert(actual == expect)
+    }
+    'showM {
+      val actual = matrix.showM mkString "\n"
+      val l1 = "1 2 3"
+      val l2 = "4 5 6"
+      val l3 = "7 8 9"
+      val expect = Seq(l1, l2, l3) mkString "\n"
       assert(actual == expect)
     }
   }
